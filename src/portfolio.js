6c1ae461086084384073e1618bd905e43c804f95
dhairyashah1 <dhairyashah110501@gmail.com>
2021-09-21 23:11:24 +0530

    Updated name info

--- src/portfolio.js
+++ src/portfolio.js
@@ -275,18 +275,31 @@ const skills = {
 const degrees = {
   degrees: [
     {
-      title: "Shantilal Shah Engineering College",
-      subtitle: "Bachelor in Information Technology",
+      title: "Veermata Jijabai Technological Institute",
+      subtitle: "Bachelor in Electronics and Telecommunication Engineering",
       logo_path: "ssec.png",
       alt_name: "SSEC",
-      duration: "2021 - Present",
+      duration: "2019 - Present",
       descriptions: [
         "⚡ I'm currently pursuing my bachelors in Information Technology.",
         // "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
         // "⚡ I have also completed various online courses for Backend , Web , Mobile App Development, etc.",
         // "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
       ],
-      website_link: "http://www.ssgec.ac.in/",
+      website_link: "https://vjti.ac.in/",
+    },
+    {
+      title: "Dr. S. & S. S. Ghandhy College Of Engineering & Technology",
+      subtitle: "Diploma in Information Technology",
+      logo_path: "ssgandhy.png",
+      alt_name: "SSGC",
+      duration: "2018 - 2021",
+      descriptions: [
+        "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        "⚡ I have also completed various online courses for Backend , Web , Mobile App Development, etc.",
+        "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
+      ],
+      website_link: "http://www.ssgc.cteguj.in/",
     },
     {
       title: "Dr. S. & S. S. Ghandhy College Of Engineering & Technology",
